d3c0d110ab93f12d49e024a2ceefd145e4bb071c
InjamulRashik <ihrashik@gmail.com>
2021-04-09 13:02:44 +0600

    Dashboard Issue Fixed!

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 document.getElementById("workshop-button").onclick = function () {
   location.href = "./Pages/workshop.html";
 };
+document.getElementById("sign-in-btn").onclick = function () {
+  location.href = "./Pages/login.html";
+};
+document.getElementById("sign-in-btn2").onclick = function () {
+  location.href = "./Pages/login.html";
+};
+document.getElementById("profile-btn").onclick = function () {
+  location.href = "./Pages/profile.html";
+};
